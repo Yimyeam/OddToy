8569828545774f7ca31a98a8cf0d0a563dd9b278
oYoiii <yimyeam_r@silpakorn.edu>
2026-09-18 15:36:00 +0700

    Com Prototype

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +12,20 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private TMP_Text scoreText;
 
+    [SerializeField]
+    private TMP_Text timeText;
+
+    [SerializeField]
+    private float time = 60f;
+
+    [SerializeField]
+    private GameObject gameOverPanel;
+
+    [SerializeField]
+    private TMP_Text finalScoreText;
+
+    private bool isGameOver;
+
     void Awake()
     {
         instance = this;
@@ -18,7 +33,26 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        Time.timeScale = 1f;
+
         scoreText.text = "SCORE: " + score;
+        timeText.text = "TIME: " + Mathf.CeilToInt(time);
+    }
+
+    void Update()
+    {
+        if (isGameOver)
+            return;
+
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            time = 0;
+            GameOver();
+        }
+
+        timeText.text = "TIME: " + Mathf.CeilToInt(time);
     }
 
     public void AddScore(int point)
@@ -26,5 +60,23 @@ public class GameManager : MonoBehaviour
         score += point;
 
         scoreText.text = "SCORE: " + score;
+    }
+
+    private void GameOver()
+    {
+        isGameOver = true;
+
+        finalScoreText.text = "FINAL SCORE: " + score;
+
+        gameOverPanel.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
